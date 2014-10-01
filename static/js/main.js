e2d5d61faefe250487840b8c30e3a19496f5ff5b
Michael Klopf <klopf.michael@gmail.com>
2014-10-01 16:15:55 +0200

    5. Checking rules.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 var $ = require('jquery');
 
 var gameBoard = [];
-var rows = 2;
-var columns = 2;
+var rows = 20;
+var columns = 20;
 var grid_width = 600;
 var grid_height = 600;
 var start_x = 50;
@@ -17,22 +17,22 @@ $(document).ready(function() {
 var initializeGameBoard = function(context) {
   createEmptyGameBoard();
 
-  //addInitialValuesToGameBoard();
+  addInitialValuesToGameBoard();
 
   paintGameBoard(context);
 
-  //setInterval(checkLife(context), 1000);
+  setInterval(function(){checkLife(context)}, 1000);
 };
 
 var checkLife = function(context) {
-    evaluateLife(context);
-    paintGameBoard(context);
+  evaluateLife(context);
+  paintGameBoard(context);
 };
 
 var evaluateLife = function(context) {
   for (var h = 0; h < rows; h++) {
     for (var w = 0; w < columns; w++) {
-        checkRulesForCell(h, w);
+      checkRulesForCell(h, w);
     }
   }
 };
@@ -43,7 +43,28 @@ var checkRulesForCell = function(row_pos, col_pos) {
     console.log("Error when checking neighbors of cell");
     return;
   }
-}
+
+  var livingNeighbors = countLivingNeighbors(neighbors);
+  // when cell is alive it can only die or survive (survice is implicit)
+  if (gameBoard[row_pos][col_pos] === 'x') {
+    if (isDyingOfOverpopulation(livingNeighbors) || isDyingOfUnderpopulation(livingNeighbors)) {
+      gameBoard[row_pos][col_pos] = '';
+      return;
+    }
+  // when cell is dead it can be born or stay dead
+  } else if (gameBoard[row_pos][col_pos] === '') {
+    if (isDyingOfOverpopulation(livingNeighbors) || isDyingOfUnderpopulation(livingNeighbors)) {
+      gameBoard[row_pos][col_pos] = '';
+      return;
+    }
+    if (isBorn(livingNeighbors)) {
+      gameBoard[row_pos][col_pos] = 'x';
+      return;
+    }
+  } else {
+    console.log("Warning: Unkown cell type.");
+  }
+};
 
 var paintGameBoard = function(context) {
   var cell_width = grid_width / columns;
@@ -52,13 +73,12 @@ var paintGameBoard = function(context) {
   var current_pos_x = start_x;
   var current_pos_y = start_y;
   context.moveTo(current_pos_x, current_pos_y);
-
   for (var h = 0; h < rows; h++) {
     for (var w = 0; w < columns; w++) {
       if (gameBoard[h][w] === '') {
-        context.rect(current_pos_x, current_pos_y, cell_width, cell_height);
+        context.clearRect(current_pos_x, current_pos_y, cell_width, cell_height);
         context.strokeStyle = "black";
-        context.stroke();
+        context.strokeRect(current_pos_x, current_pos_y, cell_width, cell_height);
       } else if (gameBoard[h][w] === 'x') {
         context.fillStyle = "black";
         context.fillRect(current_pos_x, current_pos_y, cell_width, cell_height);
@@ -86,29 +106,48 @@ var createEmptyGameBoard = function() {
 /*
   Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 */
-var isDyingOfUnderpopulation = function() {
+var isDyingOfUnderpopulation = function(livingNeighbors) {
+  if (livingNeighbors < 2)
+    return true;
+  else
     return false;
 };
 
 /*
   Any live cell with two or three live neighbours lives on to the next generation.
 */
-var isSurviving = function() {
-    return false;
-};
+//var isSurviving = function(livingNeighbors) {
+//    return false;
+//};
 
 /*
   Any live cell with more than three live neighbours dies, as if by overcrowding.
 */
-var isDyingOfOverpopulation = function() {
-  return false;
+var isDyingOfOverpopulation = function(livingNeighbors) {
+  if (livingNeighbors > 3)
+    return true
+  else
+    return false;
 };
 
 /*
   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 */
-var isBorn = function() {
-  return false;
+var isBorn = function(livingNeighbors) {
+  if (livingNeighbors === 3)
+    return true;
+  else
+    return false;
+};
+
+var countLivingNeighbors = function(neighbors) {
+  var livingNeighbors = 0;
+  for (var i=0, max_neighbors = neighbors.length; i<max_neighbors; i++) {
+    if (neighbors[i] === 'x') {
+      livingNeighbors = livingNeighbors + 1;
+    }
+  }
+  return livingNeighbors;
 };
 
 var getNeighborsOfCell = function(row_pos, col_pos) {
@@ -116,7 +155,26 @@ var getNeighborsOfCell = function(row_pos, col_pos) {
     return;
   }
   var neighbors = [];
+
   neighbors = checkForCornerCells(row_pos, col_pos);
+  if (typeof neighbors === 'undefined' || !(neighbors instanceof Array)) {
+    console.log("Error when checking neighbors of cell");
+    return;
+  }
+  if (neighbors.length > 0) {
+    return neighbors;
+  }
+
+  neighbors = checkForEdgeCells(row_pos, col_pos);
+  if (typeof neighbors === 'undefined' || !(neighbors instanceof Array)) {
+    console.log("Error when checking neighbors of cell");
+    return;
+  }
+  if (neighbors.length > 0) {
+    return neighbors;
+  }
+
+  neighbors = getCellNeighbors(row_pos, col_pos);
   if (typeof neighbors === 'undefined' || !(neighbors instanceof Array)) {
     console.log("Error when checking neighbors of cell");
     return;
@@ -145,44 +203,111 @@ var checkForCornerCells = function(row_pos, col_pos) {
     return;
   }
   var neighbors = [];
+  // top left corner
   if (row_pos-1 < 0 && col_pos-1 < 0) {
-    neighbors.push(gameBoard[row_pos][col_pos]);
     neighbors.push(gameBoard[row_pos+1][col_pos]);
     neighbors.push(gameBoard[row_pos][col_pos+1]);
     neighbors.push(gameBoard[row_pos+1][col_pos+1]);
     return neighbors;
+  // top right corner
   } else if (row_pos-1 < 0 && col_pos+1 > columns-1) {
-    neighbors.push(gameBoard[row_pos][col_pos]);
     neighbors.push(gameBoard[row_pos][col_pos-1]);
     neighbors.push(gameBoard[row_pos+1][col_pos]);
     neighbors.push(gameBoard[row_pos+1][col_pos-1]);
     return neighbors;
+  // bottom left corner
   } else if (row_pos+1 > rows-1 && col_pos-1 < 0) {
-    neighbors.push(gameBoard[row_pos][col_pos]);
     neighbors.push(gameBoard[row_pos-1][col_pos]);
     neighbors.push(gameBoard[row_pos][col_pos+1]);
     neighbors.push(gameBoard[row_pos-1][col_pos+1]);
     return neighbors;
+  // bottom right corner
   } else if (row_pos+1 > rows-1 && col_pos+1 > columns-1) {
-    neighbors.push(gameBoard[row_pos][col_pos]);
     neighbors.push(gameBoard[row_pos-1][col_pos]);
     neighbors.push(gameBoard[row_pos][col_pos-1]);
     neighbors.push(gameBoard[row_pos-1][col_pos-1]);
     return neighbors;
+  // no corner cell
+  } else {
+    return neighbors;
   }
 }
 
+var checkForEdgeCells = function(row_pos, col_pos) {
+  if (!isCellValid(row_pos, col_pos)) {
+    return;
+  }
+  var neighbors = [];
+  // left edge
+  if (col_pos === 0) {
+    neighbors.push(gameBoard[row_pos-1][col_pos]);
+    neighbors.push(gameBoard[row_pos+1][col_pos]);
+    neighbors.push(gameBoard[row_pos-1][col_pos+1]);
+    neighbors.push(gameBoard[row_pos][col_pos+1]);
+    neighbors.push(gameBoard[row_pos+1][col_pos+1]);
+    return neighbors;
+  // right edge
+  } else if (col_pos === columns-1) {
+    neighbors.push(gameBoard[row_pos-1][col_pos]);
+    neighbors.push(gameBoard[row_pos+1][col_pos]);
+    neighbors.push(gameBoard[row_pos-1][col_pos-1]);
+    neighbors.push(gameBoard[row_pos][col_pos-1]);
+    neighbors.push(gameBoard[row_pos+1][col_pos-1]);
+    return neighbors;
+  // top edge
+  } else if (row_pos === 0) {
+    neighbors.push(gameBoard[row_pos][col_pos-1]);
+    neighbors.push(gameBoard[row_pos][col_pos+1]);
+    neighbors.push(gameBoard[row_pos+1][col_pos-1]);
+    neighbors.push(gameBoard[row_pos+1][col_pos]);
+    neighbors.push(gameBoard[row_pos+1][col_pos+1]);
+    return neighbors;
+  // bottom edge
+  } else if (row_pos === rows-1) {
+    neighbors.push(gameBoard[row_pos][col_pos-1]);
+    neighbors.push(gameBoard[row_pos][col_pos+1]);
+    neighbors.push(gameBoard[row_pos-1][col_pos-1]);
+    neighbors.push(gameBoard[row_pos-1][col_pos]);
+    neighbors.push(gameBoard[row_pos-1][col_pos+1]);
+    return neighbors;
+  // no edge cells
+  } else {
+    return neighbors;
+  }
+};
+
+var getCellNeighbors = function(row_pos, col_pos) {
+  if (!isCellValid(row_pos, col_pos)) {
+    return;
+  }
+  var neighbors = [];
+  // start on top
+  neighbors.push(gameBoard[row_pos-1][col_pos]);
+  neighbors.push(gameBoard[row_pos-1][col_pos+1]);
+  neighbors.push(gameBoard[row_pos][col_pos+1]);
+  neighbors.push(gameBoard[row_pos+1][col_pos+1]);
+  neighbors.push(gameBoard[row_pos+1][col_pos]);
+  neighbors.push(gameBoard[row_pos+1][col_pos-1]);
+  neighbors.push(gameBoard[row_pos][col_pos-1]);
+  // stop on top left
+  neighbors.push(gameBoard[row_pos-1][col_pos-1]);
+  return neighbors;
+};
+
 var addInitialValuesToGameBoard = function() {
   // maybe with probability. Higher on the inside.
-  /*
+/*
   gameBoard[50][50] = 'x';
   gameBoard[50][49] = 'x';
   gameBoard[51][50] = 'x';
   gameBoard[51][49] = 'x';
-  */
+*/
   gameBoard[4][5] = 'x';
   gameBoard[5][4] = 'x';
   gameBoard[5][3] = 'x';
+  gameBoard[4][10] = 'x';
+  gameBoard[5][10] = 'x';
+  gameBoard[5][10] = 'x';
 };
 
 var logGameBoard = function() {
